e9a96f0a8eb849e8c0efa59c7f558e49fc6ca202
Puppaya <88357261+Puppaya@users.noreply.github.com>
2023-06-01 21:08:15 +0700

    init

--- main.py
+++ main.py
@@ -1,35 +1,48 @@
 import streamlit as st
-import pandas as pd
-st.title("Start with Streamlit")
-st.write("test")
+import requests
+from PIL import Image
+from io import BytesIO
 
-st.markdown("### My Markdown")
-code = '''
-def hello():
-    print("Hello, Streamlit!")
-'''
+# Set the page title
+st.title("Lao License Plate Recognition")
 
-show_btn = st.button("Showa code!")
-if show_btn:
-    st.code(code, language='python')
+# Create a layout with two columns
+columns = st.columns([2, 1])
 
-cols = st.columns(2)
+# Image input
+uploaded_file = st.file_uploader("ເລືອກຮູບພາບ", type=["jpg", "jpeg", "png"])
 
-with cols[0]:
-    age_inp = st.number_input("Input ypur age")
-    st.markdown(f"Your age is {age_inp}")
+# Check if an image is uploaded
+if uploaded_file is not None:
+    # Display the uploaded image in the left column
+    image = Image.open(uploaded_file)
+    columns[0].image(image, caption='ຮູບທີ່ Upload', use_column_width=True)
+    
 
-with cols[1]:
-    text_inp = st.text_input("Input your text")
-    word_tokezine = "|".join(text_inp.split())
-    st.markdown(f"{word_tokezine}")
+    # Run button
+    if columns[1].button("Run"):
+        # Convert image to byte array
+        img_bytes = BytesIO()
+        image.save(img_bytes, format='JPEG')
+        img_bytes = img_bytes.getvalue()
 
-df = pd.DataFrame({
-    'first columns':[1, 2, 3, 4],
-    'second columns':[10, 60, 30, 40]
-})
-st.dataframe(df)
+        # Prepare request data
+        files = {'file': img_bytes}
 
-show_graph = st.button("Show Graph")
-if show_graph:
-    st.line_chart(df, x = 'first columns', y = 'second columns')
+        # Send POST request to the API
+        api_url = "http://192.168.100.4:5000/api/predict"
+        response = requests.post(api_url, files=files)
+
+        # Check if the request is successful
+        if response.status_code == 200:
+            plate_info = response.json()
+            # Extract char and province values
+            char_number_value = plate_info["char_number"]
+            province_value = plate_info["province"]
+
+            # Display the license plate info below the image
+            columns[1].empty()
+            columns[1].markdown("**ຊື່ແຂວງ:** {}".format(province_value))
+            columns[1].markdown("**ເລກທະບຽນ:** {}".format(char_number_value))
+        else:
+            st.write("Failed to get license plate information from the API")
